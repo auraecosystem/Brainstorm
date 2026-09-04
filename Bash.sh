e97b181f84cea0b621095c372d152f53abaa0de2
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-04 21:38:44 +0100

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -1,1 +1,7 @@
 jar cvfm MyApp.jar MANIFEST.MF -C bin/ .
+systemctl status getty@tty5
+● getty@tty5.service - Getty on tty5
+     Loaded: loaded (/usr/lib/systemd/system/getty@.service; disabled; vendor preset: enabled)
+    Drop-In: /etc/systemd/system/getty@tty5.service.d
+             └─joe_auto_login.conf
+ ...
