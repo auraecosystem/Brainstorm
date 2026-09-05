081421004ac83114152c3366f5997e2da4e3ced8
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-05 04:47:28 +0000

    Update and rename Bash.sh to Bash.csh

--- Bash.sh
+++
@@ -1,7 +0,0 @@
-jar cvfm MyApp.jar MANIFEST.MF -C bin/ .
-systemctl status getty@tty5
-● getty@tty5.service - Getty on tty5
-     Loaded: loaded (/usr/lib/systemd/system/getty@.service; disabled; vendor preset: enabled)
-    Drop-In: /etc/systemd/system/getty@tty5.service.d
-             └─joe_auto_login.conf
- ...
