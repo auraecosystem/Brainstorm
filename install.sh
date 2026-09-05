95e6a7e336415ef62f20f6577695f8c1192737e0
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-05 06:19:02 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+# On macOS and Linux.
+curl -LsSf https://astral.sh/ruff/install.sh | sh
+
+# On Windows.
+powershell -c "irm https://astral.sh/ruff/install.ps1 | iex"
+
+# For a specific version.
+curl -LsSf https://astral.sh/ruff/0.16.6/install.sh | sh
+powershell -c "irm https://astral.sh/ruff/0.16.6/install.ps1 | iex"
